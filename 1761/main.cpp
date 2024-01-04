38f3a5c93d0116aaac8b11e9231cd9a456dc9df5
haebyun <bhk4392@gmail.com>
2024-01-04 23:59:33 +0900

    Child-to-LCA로 올라가며 거리를 찾는 방법 대신 Root-to-Child까지의 거리를 모두 합한 값을 저장한 배열을 추가 a와 b 사이의 거리를 찾고자 할 때 Root-to-a까지 거리 + Root-to-b까지 거리 - 2*Root-to-LCA까지 거리로 로직 변경 -> 실행 시간 감소 및 메모리를 줄이는 최적화(pair자료형을 하나 더 줄일 수 있기 때문)

--- 1761/main.cpp
+++ 1761/main.cpp
@@ -1,103 +1,95 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#include <cmath>
 
 using namespace std;
 
-const int MAX = 17;
-int N, M, res, root{1};
+const int MAX = 16;
+int N, M, res, root {1};
 vector<vector<pair<int, int>>> v;
 vector<pair<int, int>> tar;
 int depth[40001];
-pair<int, int> parent[40001][MAX];
+int parent[40001][MAX];
+int dis[40001];
 
-void getNodesDepth(int cur) {
-    for (pair<int, int> p: v[cur]) {
+void setNodesDepth(int cur) {
+    for(pair<int, int> p: v[cur]) {
         int node = p.first;
         int distance = p.second;
-        if (depth[node] == -1) {
-            parent[node][0].first = cur;
-            parent[node][0].second = distance;
-            depth[node] = depth[cur] + 1;
-            getNodesDepth(node);
+        if(depth[node]==-1) {
+            parent[node][0] = cur;
+            depth[node] = depth[cur]+1;
+            dis[node] = dis[cur]+distance;
+            setNodesDepth(node);
         }
     }
 }
 
-void getNodesParent() {
-    for (int k = 1; k < MAX; k++) {
-        for (int cur = 1; cur <= N; cur++) {
-            parent[cur][k].first = parent[parent[cur][k - 1].first][k - 1].first;
-            parent[cur][k].second = parent[parent[cur][k - 1].first][k - 1].second + parent[cur][k - 1].second;
+void setNodesParent() {
+    for(int k = 1; k < MAX; k++) {
+        for(int cur = 1; cur <= N; cur++) {
+            parent[cur][k] = parent[parent[cur][k-1]][k-1];
         }
     }
 }
 
-void update(int &cur, int diff) {
-    if (!diff) {
-        return;
+void update(int& cur, int diff) {
+    for(int i=0; i<MAX; i++) {
+        if(diff & (1<<i)) {
+            cur = parent[cur][i];
+        }
     }
-    int lim = floor(log2(diff));
-    diff -= pow(2, lim);
-    res += parent[cur][lim].second;
-    cur = parent[cur][lim].first;
-    update(cur, diff);
 }
 
-void func(int &a, int &b) {
-    for (int i = MAX - 1; i >= 0; i--) {
-        if (parent[a][i].first != 0 && parent[a][i].first != parent[b][i].first) {
-            update(a, pow(2, i));
-            update(b, pow(2, i));
-            break;
+void func(int& a, int& b) {
+    for(int i = MAX-1; i >= 0; i--) {
+        if(parent[a][i]!= parent[b][i]) {
+            a=parent[a][i];
+            b=parent[b][i];
         }
     }
 }
 
 int LCA(int a, int b) {
     //deeperNode = a
-    if (depth[a] < depth[b]) {
+    if(depth[a] < depth[b]) {
         swap(a, b);
     }
-    res = 0;
-    int diff = depth[a] - depth[b];
+    res = dis[a]+dis[b];
+    int diff = depth[a]-depth[b];
     update(a, diff);
 
-    if (a == b) {
-        return res;
+    if(a==b) {
+        return res-2*dis[a];
     }
-
-    while (parent[a][0].first != parent[b][0].first) {
-        func(a, b);
-    }
-    res += (parent[a][0].second + parent[b][0].second);
-    return res;
+    func(a,b);
+    a=parent[a][0];
+    return res-2*dis[a];
 }
 
 void solve() {
-    getNodesDepth(root);
-    getNodesParent();
-    for (pair<int, int> p: tar) {
+    setNodesDepth(root);
+    setNodesParent();
+    for(pair<int, int> p: tar) {
         cout << LCA(p.first, p.second) << '\n';
     }
 }
 
 void init() {
     cin >> N;
-    v.resize(N + 1);
-    for (int i = 0; i <= N; i++) {
+    v.resize(N+1);
+    for(int i = 0; i <= N; i++) {
         depth[i] = -1;
     }
     depth[root] = 0;
-    for (int i = 0; i < N - 1; i++) {
+    for(int i = 0; i < N-1; i++) {
         int a, b, d;
         cin >> a >> b >> d;
         v[a].push_back({b, d});
         v[b].push_back({a, d});
     }
     cin >> M;
-    for (int i = 0; i < M; i++) {
+    for(int i = 0; i < M; i++) {
         int a, b;
         cin >> a >> b;
         tar.push_back({a, b});
