6c1697c842a01c7fb430cd506e54e683532f5d27
haebyun <bhk4392@gmail.com>
2024-01-13 01:58:57 +0900

    원래 풀이에 아이디어를 얹어서 최적화

--- 15989/main.cpp
+++ 15989/main.cpp
@@ -2,32 +2,27 @@
 using namespace std;
 
 int T, N;
-int memo[10001][4];
+int memo[10001];
 
 void Solve() {
-	memo[1][1] = 1;
-	memo[2][1] = 1;
-	memo[2][2] = 1;
-	memo[3][1] = 1;
-	memo[3][2] = 1;
-	memo[3][3] = 1;
-	for (int i = 4; i <= 10000; i++) {
-		memo[i][1] = memo[i - 1][1];
-		memo[i][2] = memo[i - 2][1] + memo[i - 2][2];
-		memo[i][3] = memo[i - 3][1] + memo[i - 3][2] + memo[i - 3][3];
+	memo[0]=1;
+	for(int i=1; i<=3; i++) {
+		for(int j=i; j<=10000; j++) {
+			memo[j] += memo[j-i];
+		}
 	}
 }
 
 void Init() {
 	cin >> N;
-	cout << memo[N][1] + memo[N][2] + memo[N][3] << '\n';
+	cout << memo[N] << '\n';
 }
 
 int main() {
 	cin.tie(nullptr)->ios_base::sync_with_stdio(false);
 	Solve();
 	cin >> T;
-	while (T--) {
+	while(T--) {
 		Init();
 	}
 	return 0;
