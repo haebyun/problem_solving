c6cdc231aeba60b96fad712f6afdfbb232fae445
haebyun <bhk4392@gmail.com>
2024-02-10 15:14:23 +0900

    가독성이 좋도록 코드 변경

--- 1806/main.cpp
+++ 1806/main.cpp
@@ -7,50 +7,44 @@ int N,S,res{100001};
 vector<int> v;
 
 void Solve(){
-	if(v[0]>=S) {
-		cout<<1<<'\n';
-		return;
+  int l{0}, r{0}, sum;
+  sum = v[l];
+  while(true) {
+	if(r>=N) {
+	  break;
 	}
-	int l, r, sum;
-	l=0;
-	r=1;
-	sum = v[l] + v[r];
-	while(true) {
-		if(r>=N) {
-			break;
-		}
-		if(sum<S) {
-			r++;
-			sum += v[r];
-		}
-		else{
-			res = min(res, r-l+1);
-			if(res==1) {
-				break;
-			}
-			sum-=v[l];
-			l++;
-		}
-	}
-	if(res==100001) {
-		cout<<0<<'\n';
+	if(sum<S) {
+	  r++;
+	  sum += v[r];
 	}
 	else{
-		cout<<res<<'\n';
+	  res = min(res, r-l+1);
+	  if(res==1) {
+		break;
+	  }
+	  sum-=v[l];
+	  l++;
 	}
+  }
+  if(res==100001) {
+	cout<<0<<'\n';
+  }
+  else{
+	cout<<res<<'\n';
+  }
 }
 
 void Init(){
-	cin>>N>>S;
-	v.resize(N+1);
-	for(int i=0; i<N; i++) {
-		cin >> v[i];
-	}
+  cin>>N>>S;
+  v.resize(N+1);
+  for(int i=0; i<N; i++) {
+	cin >> v[i];
+  }
 }
 
 int main(){
-	cin.tie(nullptr)->ios_base::sync_with_stdio(false);
-	Init();
-	Solve();
-	return 0;
+  cin.tie(nullptr)->ios_base::sync_with_stdio(false);
+  Init();
+  Solve();
+  return 0;
 }
