3d7feaeb0f5552eab376da2d1c22baa379a2dd0d
haebyun <bhk4392@gmail.com>
2024-03-11 16:16:30 +0900

    백준 15311 - 생각이 나면 쉽게 풀리고, 안 나면 풀 수 없는 어이가 없는 문제... 너무 발상적이라 추천하지 않는 문제. 재미는 있습니다.

---
+++ 15311/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+void Solve(){
+    cout<<2000<<'\n';
+    for(int i=1; i<=1000; i++) {
+        cout<<1<<' ';
+    }
+    for(int i=1; i<=1000; i++) {
+        cout<<1000<<' ';
+    }
+}
+
+int main(){
+	cin.tie(nullptr)->ios_base::sync_with_stdio(false);
+	Solve();
+	return 0;
+}
